1522f9f6582c24ca466741b18e3b57b8858e2b80
5pyd3r <5pyd3r.tarsylia@gmail.com>
2016-09-14 09:34:17 +0000

    strip unrelated code

--- main.c
+++ main.c
@@ -48,17 +48,8 @@ static const char *path_last(const char *p) {
 int main(int argc, const char *argv[]) {
   int n, new_argc = 1;
   const char *execpath = argv[0];
-  const char *scriptfile = (char *)0;
-  const char *programfile = (char *)0;
-  const char *libdirs = (char *)0;
-  const char *libexts = (char *)0;
   int status;
   const char *arg;
-  int eoc = 0;
-  int optlevel = 0;
-  int debug_on_exception = 0;
-  int import_notify = 0;
-  int compile_imported_libraries = 0;
 
   if (strcmp(Skernel_version(), VERSION) != 0) {
     (void) fprintf(stderr, "unexpected shared library version %s for %s version %s\n", Skernel_version(), execpath, VERSION);
@@ -72,62 +63,6 @@ int main(int argc, const char *argv[]) {
     arg = argv[n];
     if (strcmp(arg,"--") == 0) {
       while (++n < argc) argv[new_argc++] = argv[n];
-    } else if (strcmp(arg,"--retain-static-relocation") == 0) {
-      Sretain_static_relocation();
-    } else if (strcmp(arg,"--enable-object-counts") == 0) {
-      eoc = 1;
-    } else if (strcmp(arg,"--optimize-level") == 0) {
-      const char *nextarg;
-      if (++n == argc) {
-        (void) fprintf(stderr,"%s requires argument\n", arg);
-        exit(1);
-      }
-      nextarg = argv[n];
-      if (strcmp(nextarg,"0") == 0)
-        optlevel = 0;
-      else if (strcmp(nextarg,"1") == 0)
-        optlevel = 1;
-      else if (strcmp(nextarg,"2") == 0)
-        optlevel = 2;
-      else if (strcmp(nextarg,"3") == 0)
-        optlevel = 3;
-      else {
-        (void) fprintf(stderr,"invalid optimize-level %s\n", nextarg);
-        exit(1);
-      }
-    } else if (strcmp(arg,"--debug-on-exception") == 0) {
-      debug_on_exception = 1;
-    } else if (strcmp(arg,"--import-notify") == 0) {
-      import_notify = 1;
-    } else if (strcmp(arg,"--libexts") == 0) {
-      if (++n == argc) {
-        (void) fprintf(stderr,"%s requires argument\n", arg);
-        exit(1);
-      }
-      libexts = argv[n];
-    } else if (strcmp(arg,"--libdirs") == 0) {
-      if (++n == argc) {
-        (void) fprintf(stderr,"%s requires argument\n", arg);
-        exit(1);
-      }
-      libdirs = argv[n];
-    } else if (strcmp(arg,"--compile-imported-libraries") == 0) {
-      compile_imported_libraries = 1;
-    } else if (strcmp(arg,"--help") == 0) {
-      fprintf(stderr,"usage: %s [options and files]\n", execpath);
-      fprintf(stderr,"options:\n");
-#define sep ":"
-      fprintf(stderr,"  --libdirs <dir>%s...                     set library directories\n", sep);
-      fprintf(stderr,"  --libexts <ext>%s...                     set library extensions\n", sep);
-      fprintf(stderr,"  --compile-imported-libraries            compile libraries before loading\n");
-      fprintf(stderr,"  --import-notify                         enable import search messages\n");
-      fprintf(stderr,"  --optimize-level <0 | 1 | 2 | 3>        set optimize-level\n");
-      fprintf(stderr,"  --debug-on-exception                    on uncaught exception, call debug\n");
-      fprintf(stderr,"  --enable-object-counts                  have collector maintain object counts\n");
-      fprintf(stderr,"  --retain-static-relocation              keep reloc info for compute-size, etc.\n");
-      fprintf(stderr,"  --help                                  print help and exit\n");
-      fprintf(stderr,"  --                                      pass through remaining args\n");
-      exit(0);
     } else {
       argv[new_argc++] = arg;
     }
@@ -148,40 +83,11 @@ int main(int argc, const char *argv[]) {
 #define CALL1(who, arg) Scall1(Stop_level_value(Sstring_to_symbol(who)), arg)
   CALL1("suppress-greeting", Strue);
   CALL1("waiter-prompt-string", Sstring(""));
-  if (eoc) {
-    CALL1("enable-object-counts", Strue);
-  }
-  if (optlevel != 0) {
-    CALL1("optimize-level", Sinteger(optlevel));
-  }
-  if (debug_on_exception != 0) {
-    CALL1("debug-on-exception", Strue);
-  }
-  if (import_notify != 0) {
-    CALL1("import-notify", Strue);
-  }
-  if (libdirs == 0) libdirs = getenv("CHEZSCHEMELIBDIRS");
-  if (libdirs != 0) {
-    CALL1("library-directories", Sstring(libdirs));
-  }
-  if (libexts == 0) libexts = getenv("CHEZSCHEMELIBEXTS");
-  if (libexts != 0) {
-    CALL1("library-extensions", Sstring(libexts));
-  }
-  if (compile_imported_libraries != 0) {
-    CALL1("compile-imported-libraries", Strue);
-  }
 
-  if (scriptfile != (char *)0)
-   /* Sscheme_script invokes the value of the scheme-script parameter */
-    status = Sscheme_script(scriptfile, new_argc, argv);
-  else if (programfile != (char *)0)
-   /* Sscheme_script invokes the value of the scheme-script parameter */
-    status = Sscheme_program(programfile, new_argc, argv);
-  else {
-   /* Sscheme_start invokes the value of the scheme-start parameter */
-    status = Sscheme_start(new_argc, argv);
-  }
+  Senable_expeditor("");
+  
+  /* Sscheme_start invokes the value of the scheme-start parameter */
+  status = Sscheme_start(new_argc, argv);
 
  /* must call Scheme_deinit after saving the heap and before exiting */
   Sscheme_deinit();
